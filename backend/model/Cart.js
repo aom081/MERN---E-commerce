94ce05f57b8db29fa51d1b183b771bdbcefaca3e
aom081 <matemsom@gmail.com>
2025-02-05 16:34:48 +0700

    add doc swagger cart

--- backend/model/Cart.js
+++ backend/model/Cart.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const CartSchema = new Schema({
-  ProductId: { type: Schema.Types.ObjectId, ref: "Product" },
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  price: { type: Number, required: true },
-  image: { type: String, required: true },
-  quantity: { type: String, required: true },
-});
+const CartSchema = new Schema(
+  {
+    ProductId: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    quantity: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const ProductModel = module("Product", ProductSchema);
-module.exports = ProductModel;
+const CartModel = module("Cart", CartSchema);
+module.exports = CartModel;
