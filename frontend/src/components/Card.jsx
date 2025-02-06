489e9c14a9721445be1e0064012a96ac62630960
aom081 <matemsom@gmail.com>
2025-02-06 12:30:33 +0700

    update add to cart

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,86 +1,116 @@
-import React, { useState, useContext } from "react";
-import { useQuery } from "@tanstack/react-query";
+import Profile from "./Profile";
 import { AuthContext } from "../context/Authcontext";
-import CartService from "../service/cart.service";
-import useCart from "../hook/useCart";
-import Swal from "sweetalert2";
-
-const Card = ({ item }) => {
-  const { id, name, image, description, category, price } = item;
-  const {user} = useContext(AuthContext);
-  const [cart,refetch] = useCart();
-  const [isHeartFiled, setIsHeartFiled] = useState(false);
-  const handleHeartClick = () => {
-    setIsHeartFiled(!isHeartFiled);
-  };
-
-  const handleAddToCart = async () => {
-    if (!user || !user.email) {
-      Swal.fire({
-        title: "Error",
-        text: "Please login to add item to cart",
-        icon: "error",
-        });
-        return;
-    }
-    try {
-      const cartItem = {
-        productId: id,
-        email: user.email,
-        quantity: 1,
-        name,
-        price,
-        image,
-      }
-      const response = await CartService.createCartItem(cartItem);
-      if (response.status === 200) {
-        Swal.fire({
-          title: "Success",
-          text: "Item added to cart successfully",
-          icon: "success",
-          });
-          refetch();
-      }
-    } catch (error) {
-      Swal.fire({
-        title: "Error",
-        text: "Failed to add item to cart",
-        icon: "error",
-      })
-    }
-  }
+import { useContext } from "react";
+import UserIcon from "./icons/UserIcon";
+import Modal from "./Modal";
+import useCart from "../hooks/useCart";
+import { FaUserCircle } from "react-icons/fa";
+const NavBar = () => {
+  const { user } = useContext(AuthContext);
+  const [cart, refetch] = useCart();
+  const navItems = (
+    <>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li tabIndex={0}>
+        <details>
+          <summary>Category</summary>
+          <ul>
+            <li>
+              <a href="/shop">All</a>
+            </li>
+            <li>
+              <a href="/shop?category=clothing">Clothing</a>
+            </li>
+            <li>
+              <a href="/shop?category=accessories">Accessories</a>
+            </li>
+            <li>
+              <a href="/shop?category=gadget">Gadgets</a>
+            </li>
+            <li>
+              <a href="/shop?category=swag">Swag</a>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li tabIndex={0}>
+        <details>
+          <summary>Services</summary>
+          <ul>
+            <li>
+              <a href="">Order Online</a>
+            </li>
+            <li>
+              <a href="">Order Tracking</a>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <a href="/">Promotions</a>
+      </li>
+      <li>
+        <a href="/">About Us</a>
+      </li>
+    </>
+  );
   return (
-    <div className="card shadow-xl relative mr-5 md:my-5 h-120">
-      <div
-        className="rating gap-1 absolute right-2 top-2 p-4 z-10 heartStart"
-        onClick={handleHeartClick}
-      >
-        <input
-          type="radio"
-          name="heart"
-          className={`mask mask-heart ${isHeartFiled ? "bg-red-400" : " "}`}
-        />
-      </div>
-      <figure>
-        <img
-          src={image}
-          alt={name}
-          className="hover:scale-105 transition-all duration-300 md:h-30"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{name}</h2>
-        <p>{description}</p>
-        <div className="card-action justify-between items-center mt-2">
-          <h5 className="text-lg font-bold">
-            {price}
-            <span className="text-red">฿</span>
-          </h5>
-          <button className="btn bg-red text-white">Add to cart</button>
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              {navItems}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl" href="/">
+            <img src="/logo.png" alt="" className="h-6 lg:h-12 pr-1 mx-auto" />
+            SE Souvenir Shop
+          </a>
         </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navItems}</ul>
+        </div>
+        <div className="navbar-end">
+          {/* Ternary operator */}
+          {user ? (
+            <>
+              <Profile />
+            </>
+          ) : (
+            <button
+              className="btn bg-red text-white rounded-full px-5 flex items-center gap-2"
+              onClick={() => document.getElementById("login").showModal()}
+            >
+              <FaUserCircle className="w-6 h-6" /> Login
+            </button>
+          )}
+        </div>
+        <Modal name="login" />
       </div>
     </div>
   );
 };
 
-export default Card;
+export default NavBar;
